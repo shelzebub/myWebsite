3540ffdd905c05a39d0a26781539bc4d71e5cceb
Shely <makaxsole@gmail.com>
2019-06-28 17:00:38 -0700

    fixed footer, cleaned up code, added cv

--- js/index.js
+++ js/index.js
@@ -1,102 +1,97 @@
 //Global Variables
 var currentYear = new Date().getFullYear();
+var copyRight =
+  "&copy;" + currentYear + " Shely Lin | All rights reserved | Software Developer | Web Designer | Surrey, BC";
 
 // shrinking navbar
 function checkForNavShrink(scrolledPixels) {
-    if (screen.width <= 699) {
-        let navbar = $('nav.navbar');
-        if (scrolledPixels > 300) {
-            navbar.find('#logo img, .nav-link, #logoName').addClass('shrink')
-        } else {
-            navbar.find('#logo img, .nav-link, #logoName').removeClass('shrink')
-        }
+  if (screen.width <= 699) {
+    let navbar = $("nav.navbar");
+    if (scrolledPixels > 300) {
+      navbar.find("#logo img, .nav-link, #logoName").addClass("shrink");
     } else {
-        let navbar = $('nav.navbar');
-        if (scrolledPixels > 900) {
-            navbar.find('#logo img, .nav-link, #logoName').addClass('shrink')
-        } else {
-            navbar.find('#logo img, .nav-link, #logoName').removeClass('shrink')
-        }
+      navbar.find("#logo img, .nav-link, #logoName").removeClass("shrink");
     }
+  } else {
+    let navbar = $("nav.navbar");
+    if (scrolledPixels > 900) {
+      navbar.find("#logo img, .nav-link, #logoName").addClass("shrink");
+    } else {
+      navbar.find("#logo img, .nav-link, #logoName").removeClass("shrink");
+    }
+  }
 }
 
 function checkForMobileNavShrink(scrolledPixels) {
-    let navbar = $('#navHome');
-    if (scrolledPixels > 900) {
-        navbar.addClass('shrink')
-    } else {
-        navbar.removeClass('shrink')
-    }
+  let navbar = $("#navHome");
+  if (scrolledPixels > 900) {
+    navbar.addClass("shrink");
+  } else {
+    navbar.removeClass("shrink");
+  }
 }
 
 // loading handlers
 function initializeEvents() {
-    //scroll listener
-    $(window).scroll(function() {
-        // shrinking navbar
-        checkForNavShrink($(this).scrollTop());
-        checkForMobileNavShrink($(this).scrollTop());
-    });
+  //scroll listener
+  $(window).scroll(function() {
+    // shrinking navbar
+    checkForNavShrink($(this).scrollTop());
+    checkForMobileNavShrink($(this).scrollTop());
+  });
 
-    // onclick listener
-    $('#logo').click(() => {
-        window.scrollTo({
-            'behavior': 'smooth',
-            'left': 0,
-            'top': 0
-          });
+  // onclick listener
+  $("#logo").click(() => {
+    window.scrollTo({
+      behavior: "smooth",
+      left: 0,
+      top: 0
     });
-    $('.nav-home').click(() => {
-        window.scrollTo({
-            'behavior': 'smooth',
-            'left': 0,
-            'top': 0
-          });
+  });
+  $(".nav-home").click(() => {
+    window.scrollTo({
+      behavior: "smooth",
+      left: 0,
+      top: 0
     });
-    $('#navMyWorks').click(() => {
-        window.scrollTo({
-            'behavior': 'smooth',
-            'left': 0,
-            'top': document.querySelector('#myWorks').offsetTop
-          });
+  });
+  $("#navMyWorks").click(() => {
+    window.scrollTo({
+      behavior: "smooth",
+      left: 0,
+      top: document.querySelector("#myWorks").offsetTop
     });
-    $('#navAboutMe').click(() => {
-        window.scrollTo({
-            'behavior': 'smooth',
-            'left': 0,
-            'top': document.querySelector('#aboutMe').offsetTop
-          });
+  });
+  $("#navAboutMe").click(() => {
+    window.scrollTo({
+      behavior: "smooth",
+      left: 0,
+      top: document.querySelector("#aboutMe").offsetTop
     });
-    $('#navContactMe').click(() => {
-        window.scrollTo({
-            'behavior': 'smooth',
-            'left': 0,
-            'top': document.querySelector('#contactMe').offsetTop
-          });
+  });
+  $("#navContactMe").click(() => {
+    window.scrollTo({
+      behavior: "smooth",
+      left: 0,
+      top: document.querySelector("#contactMe").offsetTop
     });
-    $('#storyBtn').click(() => {
-        window.scrollTo({
-            'behavior': 'smooth',
-            'left': 0,
-            'top': document.querySelector('#myStory').offsetTop
-          });
-    });
+  });
 
-    // icon bounce animation on hover
-    $(".icon-img").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function(){
-        $(this).removeClass("animate-bounce")
-    })
-    
-    $(".icon-img").hover(function(){
-        $(this).addClass("animate-bounce");
-    })
+  // icon bounce animation on hover
+  $(".icon-img").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function() {
+    $(this).removeClass("animate-bounce");
+  });
+
+  $(".icon-img").hover(function() {
+    $(this).addClass("animate-bounce");
+  });
 }
 
 $(function() {
-    window.onbeforeunload = function () {
-        window.scrollTo(0, 0);
-    }
-    
-    initializeEvents();
-});
+  window.onbeforeunload = function() {
+    window.scrollTo(0, 0);
+  };
 
+  initializeEvents();
+  $("#copyRight").html(copyRight);
+});
